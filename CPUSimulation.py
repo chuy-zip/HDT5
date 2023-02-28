a5ab2c272bf363a68ead879c370cf911b5ebecbb
chuy-zip <andreschuy03@gmail.com>
2023-02-28 15:49:58 -0600

    Program version 2.0

--- CPUSimulation.py
+++ CPUSimulation.py
@@ -6,31 +6,60 @@ import numpy as np
 START_TIMES = []
 END_TIMES = []
 
-#def CPUProc_Generator(env, Qty, Interval, RAM, CPUs, InstrP_S):
+def CPUProc_Generator(env, Qty, Interval, RAM, CPUs, InstrP_S):
 
-#    for i in range(1 , Qty + 1):
- #       yield  env.timeout(random.expovariate(1.0/Interval))
-  #      random_Inst = random.randint(1, 10)
-   #     random_Mem = random.randint(1, 10)
-#
- #       print("%s creado en %.1f con memoria de %i con %i instrucciones" % ("Proceso %d" % i, env.now,random_Mem, random_Inst))
-  #      START_TIMES.append(round(env.now, 1))
-#
- #       if(RAM.level - random_Mem >= 0):
-  #          yield RAM.get(random_Mem)
-   #         env.process(CpuProcessReady(env, "Proceso %d" % i, random_Mem, random_Inst, RAM, CPUs, InstrP_S))
+    ### Generating Cpu process with an interval
+    for i in range(1 , Qty + 1):
+        yield  env.timeout(random.expovariate(1.0/Interval))
+        random_Inst = random.randint(1, 10)
+        random_Mem = random.randint(1, 10)
 
-#        else:
- #           print("Memoria insuficiente para el %s, agregado a la lista de espera en %.1f" % ("Proceso %d" % i, env.now))
-  #          env.process(ProcessWaitingforRam(env, "Proceso %d" % i, random_Mem, random_Inst, RAM, CPUs, InstrP_S) )
+        print("%s creado en %.1f con memoria de %i con %i instrucciones" % ("Proceso %d" % i, env.now,random_Mem, random_Inst))
+        START_TIMES.append(round(env.now, 1))
 
 
-#def CpuProcessReady(env, name, memory, Instructions, RAM, CPUs, InstrP_S):
- #  print("%s ha pasado a la ram y esta listo para ejecutarse en %.1f" % (name, env.now))
-  # with CPUs.request() as req:
-   #    yield req
-    #   env.process(ProcessRunning(env, name, memory, Instructions, RAM, CPUs, req, InstrP_S))
+        yield RAM.get(random_Mem) #Waiting for available ram
 
+        ##Program ready to go to ram
+        env.process(CpuProcessReady(env, "Proceso %d" % i, random_Mem, random_Inst, RAM, CPUs, InstrP_S))
+
+        #else:
+         #   print("Memoria insuficiente para el %s, agregado a la lista de espera en %.1f" % ("Proceso %d" % i, env.now))
+          #  env.process(ProcessWaitingforRam(env, "Proceso %d" % i, random_Mem, random_Inst, RAM, CPUs, InstrP_S) )
+
+##Process ready to ask for cpu
+def CpuProcessReady(env, name, memory, Instructions, RAM, CPUs, InstrP_S):
+   print("%s ha pasado a la ram y esta listo para ejecutarse en %.1f" % (name, env.now))
+   with CPUs.request() as req:
+       yield req
+       while Instructions > 0:
+            Instructions = Instructions - InstrP_S
+
+            if(Instructions <= 0 ):
+                yield env.timeout(InstrP_S)
+                print("%s terminado en %.1f" % (name, env.now))
+                END_TIMES.append(round(env.now, 1))
+                yield RAM.put(memory)
+                
+                
+            else:
+                rand_Status = random.randint(1, 2)
+
+                if(rand_Status == 2):
+                    print("%s ejecutado, ahora tiene %d instrucciones y regresa directamente a la cola de procesos en %d" % (name, Instructions, env.now))        
+                    
+                
+                elif(rand_Status == 1):
+                    CPUs.release(req)
+                    print("%s ejecutado y puesto en espera, ahora tiene %d instrucciones y regresa a la cola waiting en %d" % (name, Instructions, env.now))                                
+                    yield env.timeout(3)
+
+                    print("%s sale de la cola de espera, ahora tiene %d instrucciones y regresa a la cola de procesos en %d" % (name, Instructions, env.now))        
+                   
+
+
+
+       
 #def ProcessRunning(env, name, memory, Instructions, RAM, CPUs, req, InstrP_S):
     
  #   Instructions = Instructions - InstrP_S
@@ -59,16 +88,6 @@ END_TIMES = []
              #yield env.process(CpuProcessReady(env, name, memory, Instructions, RAM, CPUs, InstrP_S))
 
 
-#def ProcessWaitingforRam (env, name, memory, Instructions, RAM, CPUs, InstrP_S):
- #   if(RAM.level - memory >= 0):
-  #      print("Memoria disponible, Ingresando %s en %d" % (name, env.now))
-   #     env.process(CpuProcessReady(env, name, memory, Instructions, RAM, CPUs , InstrP_S))
-    
-    #else:
-     #   print("%s esperando por espacio de ram en %d" % (name , env.now))
-      #  yield env.timeout(2) #Check every cicle for available memory
-        
-
 #########################################################################################################
 print("Bienvenido al simulador de procesos en un CPU, por favor ingrese los siguientes datos")
 
@@ -79,6 +98,7 @@ InstrP_S = int(input("Que tan rapido sera el procesador (Cuantas instrucciones p
 ProccesorsQty = int(input("Cuantos procesadores se usaran\n"))
 Interval = int(input("Cual sera el intervalo para la creacion de procesos\n"))
 
+print()
 print(ProcessesQty)
 print(RAMSpace)
 print(InstrP_S)

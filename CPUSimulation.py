6c81f4bf6880a92c25bd21cace3e59dcbd7b0078
chuy-zip <andreschuy03@gmail.com>
2023-02-28 21:48:41 -0600

    Commented to fix program again

--- CPUSimulation.py
+++ CPUSimulation.py
@@ -7,21 +7,21 @@ START_TIMES = []
 END_TIMES = []
 CRUN_TIME = []
 
-def CPUProc_Generator(env, Qty, Interval, RAM, CPUs, InstrP_S):
+#def CPUProc_Generator(env, Qty, Interval, RAM, CPUs, InstrP_S):
 
     ### Generating Cpu process with an interval
-    for i in range(1 , Qty + 1):
-        yield  env.timeout(random.expovariate(1.0/Interval))
-        random_Inst = random.randint(1, 10)
-        random_Mem = random.randint(1, 10)
+    #for i in range(1 , Qty + 1):
+        #yield  env.timeout(random.expovariate(1.0/Interval))
+        #random_Inst = random.randint(1, 10)
+        #random_Mem = random.randint(1, 10)
 
-        print("%s creado en %.1f con memoria de %i con %i instrucciones" % ("Proceso %d" % i, env.now,random_Mem, random_Inst))
-        start = round(env.now, 1)
+        #print("%s creado en %.1f con memoria de %i con %i instrucciones" % ("Proceso %d" % i, env.now,random_Mem, random_Inst))
+        #start = round(env.now, 1)
 
-        yield RAM.get(random_Mem) #Waiting for available ram
+        #yield RAM.get(random_Mem) #Waiting for available ram
 
         ##Program ready to go to ram
-        env.process(CpuProcessReady(env, "Proceso %d" % i, random_Mem, random_Inst, RAM, CPUs, InstrP_S, start))
+        #env.process(CpuProcessReady(env, "Proceso %d" % i, random_Mem, random_Inst, RAM, CPUs, InstrP_S, start))
 
 ##Process ready to ask for cpu
 def CpuProcessReady(env, name, memory, Instructions, RAM, CPUs, InstrP_S, start):
@@ -29,7 +29,7 @@ def CpuProcessReady(env, name, memory, Instructions, RAM, CPUs, InstrP_S, start)
    ##Queue for Cpu proceseses, it while only execute once the processor is free
    with CPUs.request() as req:
        yield req
-       while Instructions > 0: #Process will be 
+       while Instructions > 0:  
             
             Instructions = Instructions - InstrP_S
             
@@ -77,7 +77,11 @@ env = simpy.Environment()
 RAM = simpy.Container(env, init = RAMSpace, capacity = RAMSpace)  
 CPUs = simpy.Resource(env, capacity = ProccesorsQty)
 
-env.process(CPUProc_Generator(env, ProcessesQty, Interval, RAM, CPUs, InstrP_S))
+for i in range():
+    tempInterval = (random.expovariate(1(Interval)))
+    random_Inst = random.randint(1, 10)
+    random_Mem = random.randint(1, 10)
+    env.process(CpuProcessReady(env, ProcessesQty, Interval, RAM, CPUs, InstrP_S))
 env.run()
 
 print("Tiempos de ejecuacion de cada proceso: ")

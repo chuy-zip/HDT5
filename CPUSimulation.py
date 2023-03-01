636df04e575f1ffa699be7b3df1edfb73d3f23b0
chuy-zip <andreschuy03@gmail.com>
2023-02-28 18:59:15 -0600

    Fixxed timeouts

--- CPUSimulation.py
+++ CPUSimulation.py
@@ -5,6 +5,7 @@ import numpy as np
 #Global value
 START_TIMES = []
 END_TIMES = []
+CRUN_TIME = []
 
 def CPUProc_Generator(env, Qty, Interval, RAM, CPUs, InstrP_S):
 
@@ -15,15 +16,15 @@ def CPUProc_Generator(env, Qty, Interval, RAM, CPUs, InstrP_S):
         random_Mem = random.randint(1, 10)
 
         print("%s creado en %.1f con memoria de %i con %i instrucciones" % ("Proceso %d" % i, env.now,random_Mem, random_Inst))
-        START_TIMES.append(round(env.now, 1))
+        start = round(env.now, 1)
 
         yield RAM.get(random_Mem) #Waiting for available ram
 
         ##Program ready to go to ram
-        env.process(CpuProcessReady(env, "Proceso %d" % i, random_Mem, random_Inst, RAM, CPUs, InstrP_S))
+        env.process(CpuProcessReady(env, "Proceso %d" % i, random_Mem, random_Inst, RAM, CPUs, InstrP_S, start))
 
 ##Process ready to ask for cpu
-def CpuProcessReady(env, name, memory, Instructions, RAM, CPUs, InstrP_S):
+def CpuProcessReady(env, name, memory, Instructions, RAM, CPUs, InstrP_S, start):
    print("%s ha pasado a la ram y esta listo para ejecutarse en %.1f" % (name, env.now))
    ##Queue for Cpu proceseses, it while only execute once the processor is free
    with CPUs.request() as req:
@@ -34,7 +35,9 @@ def CpuProcessReady(env, name, memory, Instructions, RAM, CPUs, InstrP_S):
             if(Instructions <= 0 ):
                 yield env.timeout(InstrP_S)
                 print("%s terminado en %.1f" % (name, env.now))
-                END_TIMES.append(round(env.now, 1))
+                end = round(env.now, 1)
+                CRUN_TIME.append(round(end - start, 1))
+
                 yield RAM.put(memory)
                 
             else:
@@ -42,13 +45,13 @@ def CpuProcessReady(env, name, memory, Instructions, RAM, CPUs, InstrP_S):
 
                 if(rand_Status == 2):
                     CPUs.release(req)
+                    yield env.timeout(InstrP_S)
                     print("%s ejecutado, ahora tiene %d instrucciones y regresa directamente a la cola de procesos en %d" % (name, Instructions, env.now))        
                 
                 elif(rand_Status == 1):
                     CPUs.release(req)
                     print("%s ejecutado y puesto en espera, ahora tiene %d instrucciones y regresa a la cola waiting en %d" % (name, Instructions, env.now))                                
                     yield env.timeout(3)
-
                     print("%s sale de la cola de espera, ahora tiene %d instrucciones y regresa a la cola de procesos en %d" % (name, Instructions, env.now))        
 
 print("Bienvenido al simulador de procesos en un CPU, por favor ingrese los siguientes datos")
@@ -76,19 +79,8 @@ CPUs = simpy.Resource(env, capacity = ProccesorsQty)
 env.process(CPUProc_Generator(env, ProcessesQty, Interval, RAM, CPUs, InstrP_S))
 env.run()
 
-print(f"Tiempos inicio: {START_TIMES}")
-print(f"Tiempos Salida: {END_TIMES}")
+print("Tiempos de ejecuacion de cada proceso: %d " % CRUN_TIME)
 
-RUN_TIMES = []
+print("Tiempo promedio de los procesos hasta salir nuevo: %d " % np.average(CRUN_TIME))
 
-print(len(START_TIMES))
-print(len(END_TIMES))
-
-for i in range (len(START_TIMES)):
-   RUN_TIMES.append(round(END_TIMES[i] - START_TIMES[i], 1))
-
-print(RUN_TIMES)
-
-print("Tiempo promedio de los procesos hasta salir: %d " % np.average(RUN_TIMES))
-
-print("Desviacion estandar de los tiempos de los procesos: %d" % np.std(RUN_TIMES))
+print("Desviacion estandar de los tiempos de los procesos nuevo: %d" % np.std(CRUN_TIME))
